120c0e5770d0f7a642c8f7aa66f8811254878cc0
Rico Chen <xiakingc@hotmail.com>
2012-01-08 22:48:08 -0500

    cosmetics change: added extra space in array_map() line

--- OOP/SimpleTemplate.php
+++ OOP/SimpleTemplate.php
@@ -10,7 +10,7 @@
 		}
 
 		public function gen_msg($arr) {
-			$keys=array_map( 'build_key' , array_keys($arr));
+			$keys=array_map( 'build_key' , array_keys($arr) );
 			return str_replace( $keys, array_values($arr), $this->_template );
 		}
 	}
